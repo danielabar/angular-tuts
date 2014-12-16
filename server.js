40385e0707c477fe5ed0b5017c66c7ce091ef341
Daniela Baron <baron.daniela@gmail.com>
2014-12-16 18:25:35 -0500

    add dev logging

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
+  morgan = require('morgan'),
   app = express();
 
 app
+  .use(morgan('dev'))
   .use(express.static('./public'))
   .get('*', function(req, res) {
     res.sendFile('public/main.html', { root: __dirname });
