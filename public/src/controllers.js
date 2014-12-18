42bcd3c943494d941d0bb8ff09d66b414559719d
Daniela Baron <baron.daniela@gmail.com>
2014-12-17 19:18:22 -0500

    display contacts list wip

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,4 +1,22 @@
 angular.module('ContactsApp')
-  .controller('ListController', function($scope) {
-    $scope.contacts = [];
+  .controller('ListController', function($scope, Contact, $location) {
+
+    // Get all contacts
+    $scope.contacts = Contact.query();
+
+    // Control what fields are displayed (later will let user set this)
+    $scope.fields = ['firstName', 'lastName'];
+
+    $scope.sort = function(field) {
+      $scope.sort.field = field;
+      $scope.sort.order = !$scope.sort.order;
+    };
+
+    $scope.sort.field = 'firstName';
+    $scope.sort.order = false;
+
+    $scope.show = function(id) {
+      $location.url('/contact/' + id);
+    };
+
   });
