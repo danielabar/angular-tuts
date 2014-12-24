4f2b3d88f601376efc71d17bbd0efb159c7efa0c
Daniela Baron <baron.daniela@gmail.com>
2014-12-24 18:42:22 -0500

    settings

--- public/src/app.js
+++ public/src/app.js
@@ -13,10 +13,22 @@ angular.module('ContactsApp', ['ngRoute', 'ngResource', 'ngMessages'])
         controller: 'SingleController',
         templateUrl: 'views/single.html'
       })
+      .when('/settings', {
+        controller: 'SettingsController',
+        templateUrl: 'views/settings.html'
+      })
       .otherwise({
         redirectTo: '/contacts'
       });
 
       // So we don't get ugly hash bang in urls
       $locationProvider.html5Mode(true);
-  });
+  })
+  .value('options', {})
+
+  // run block runs immediately when application is started (runs only once?)
+  .run(function(options, Fields) {
+    Fields.get().success(function(data) {
+      options.displayed_fields = data;
+    });
+  })
--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,11 +1,12 @@
 angular.module('ContactsApp')
-  .controller('ListController', function($scope, $rootScope, Contact, $location) {
+  .controller('ListController', function($scope, $rootScope, Contact, $location, options) {
     $rootScope.PAGE = "all";
     // Get all contacts
     $scope.contacts = Contact.query();
 
     // Control what fields are displayed (later will let user set this)
-    $scope.fields = ['firstName', 'lastName'];
+    console.log('options.displayed_fields: ' + JSON.stringify(options.displayed_fields));
+    $scope.fields = ['firstName', 'lastName'].concat(options.displayed_fields);
 
     $scope.sort = function(field) {
       $scope.sort.field = field;
@@ -44,10 +45,33 @@ angular.module('ContactsApp')
     }
   })
   .controller('SingleController', function($scope, $rootScope, $location, Contact, $routeParams) {
-    $rootScope.PAGE = "edit";
+    $rootScope.PAGE = 'edit';
+
     $scope.contact = Contact.get({ id : parseInt($routeParams.id, 10) });
     $scope.delete = function() {
       $scope.contact.$delete();
       $location.url('/contacts');
+    }
+  })
+  .controller('SettingsController', function($scope, $rootScope, options, Fields) {
+    $rootScope.PAGE = 'settings';
+
+    $scope.allFields = [];
+    $scope.fields = options.displayed_fields;
+
+    Fields.headers().then(function(data) {
+      $scope.allFields = data;
+    });
+
+    $scope.toggle = function(field) {
+      var i = options.displayed_fields.indexOf(field);
+      if (i > -1) {
+        // it's already in options, therefore this means user wants to remove it
+        options.displayed_fields.splice(i, 1);
+      } else {
+        // add it
+        options.displayed_fields.push(field);
+      }
+      Fields.set(options.displayed_fields);
     }
   });
