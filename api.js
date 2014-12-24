45a63afa6f9d9002ab7e7e73f523e4e21bbce09a
Daniela Baron <baron.daniela@gmail.com>
2014-12-24 13:17:33 -0500

    fix api wrt by id

--- api.js
+++ api.js
@@ -27,26 +27,17 @@ router
     });
 
 router
-  // This middleware only runs when there is an id
-  .param('id', function(req, res, next) {
-    // This query will be available in all the routes that have id
-    req.dbQuery = {id : parseInt(req.params.id, 10)};
-    console.log('id middleware matched, req.dbQuery: ' + JSON.stringify(req.dbQuery));
-  })
   .route('/contact/:id')
     .get(function(req, res) {
-      db.findOne(req.dbQuery, function(err, data) {
+      db.findOne(parseInt(req.params.id, 10), function(err, data) {
         res.json(data);
       });
     })
     .put(function(req, res) {
       var contact = req.body;
-      console.log('put by id route matched, contact: ' + JSON.stringify(contact));
-      // remove promise objects set by angular - wtf??
       delete contact.$promise;
       delete contact.$resolved;
-      db.update(req.dbQuery, contact, function(err, data) {
-        console.log('update data: ' + JSON.stringify(data));
+      db.update(parseInt(req.params.id, 10), contact, function(err, data) {
         // db.update supports updating array of items, but we only updated one so return that first one
         res.json(data[0]);
       });
